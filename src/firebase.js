264ede9448e429905d9fc5019ce9131b3191139b
amanda-w <amandanwong@gmail.com>
2023-04-22 20:19:21 -0700

    add: header component

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: "clone-2a8a8.firebaseapp.com",
+    projectId: "clone-2a8a8",
+    storageBucket: "clone-2a8a8.appspot.com",
+    messagingSenderId: "419600357268",
+    appId: "1:419600357268:web:ea03f327148612e3792181"
+  };
